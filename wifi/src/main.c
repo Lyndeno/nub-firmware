526032cbbd26e15e2b22398466d4b52c536f0852
Lyndon Sanche <lsanche@lyndeno.ca>
2021-01-15 12:48:53 -0700

    Add todos for code work

--- wifi/src/main.c
+++ wifi/src/main.c
@@ -1,19 +1,22 @@
 #include <ESP8266WiFi.h>
 
-const char *ssid = "NUB";
-const char *password = "capstone1234";
+const char *ap_ssid = "NUB";
+const char *ap_password = "capstone1234";
 
 // Initial setup code with assistance from https://tttapa.github.io/ESP8266/Chap07%20-%20Wi-Fi%20Connections.html
 void setup() {
     Serial.begin(115200); // this value is to be decided
-    delay(10);
+    //delay(5);
     Serial.println('\n');
 
-    WiFi.softAP(ssid, password);
+    WiFi.softAP(ap_ssid, ap_password);
+    
+    // TODO: get this codeblock into one println statement
     Serial.print("Access Point \"");
-    Serial.print(ssid);
+    Serial.print(ap_ssid);
     Serial.println("\" started");
 
+    // TODO: same with this
     Serial.print("IP Address:\t");
     Serial.println(WiFi.softAPIP());
 }
