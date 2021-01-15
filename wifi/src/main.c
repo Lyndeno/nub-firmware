29182dea4ac5a3d067cccae222ff167f2490f5f8
Lyndon Sanche <lsanche@lyndeno.ca>
2021-01-15 11:41:19 -0700

    Added reference link

--- wifi/src/main.c
+++ wifi/src/main.c
@@ -3,7 +3,7 @@
 const char *ssid = "NUB";
 const char *password = "capstone1234";
 
-// Initial setup code with assistance from 
+// Initial setup code with assistance from https://tttapa.github.io/ESP8266/Chap07%20-%20Wi-Fi%20Connections.html
 void setup() {
     Serial.begin(115200); // this value is to be decided
     delay(10);
