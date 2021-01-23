e41eb5d6275a8b889615a6863202da51124d460c
Lyndon Sanche <lsanche@lyndeno.ca>
2021-01-23 16:09:39 -0700

    MCU: Initial Code

---
+++ mcu/src/io.h
@@ -0,0 +1,31 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+#ifndef LIBIO_H
+#define LIBIO_H
+
+#define DDR(x) (*(&x - 1))     // address of data direction register of port x
+#define PORT(x) (*(&x)) 
+#define PIN(x) (*(&x - 2)) 
+
+/* Code from ECE312 project, adapt as needed
+#define PORT_LR PORTD
+
+#define PORT_ENTER PORTB
+
+#define PORT_BUZZER PORTD
+
+#define PIN_LEFT PORTD6
+
+#define PIN_RIGHT PORTD7
+
+#define PIN_ENTER PORTB0
+
+#define PIN_BUZZER PORTD5
+*/
+
+#ifndef F_CPU
+#define F_CPU 1000000
+#endif
+
+#endif
---
+++ mcu/src/main.c
@@ -0,0 +1,15 @@
+#include <avr/io.h>
+
+// Might need this
+//#include <avr/pgmspace.h>
+
+// Figure this out
+//#define F_CPU 1000000
+
+int main (void) {
+
+}
+
+void gpio_init (void) {
+    
+}
