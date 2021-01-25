668cb133477d92b09dc0dcd47aa8f94ef2e98c12
Lyndon Sanche <lsanche@lyndeno.ca>
2021-01-25 12:47:29 -0700

    Add code for turning off usart power reduction

--- mcu/src/main.c
+++ mcu/src/main.c
@@ -1,9 +1,9 @@
 #include <avr/io.h>
 
-// Might need this
+// TODO: Might need this
 //#include <avr/pgmspace.h>
 
-// Figure this out
+// TODO: Figure this out
 //#define F_CPU 1000000
 
 int main (void) {
@@ -11,5 +11,6 @@ int main (void) {
 }
 
 void gpio_init (void) {
-    
+    // Disable power reduction for USARTS
+    PRR0 |= (1<<PRUSART0)|(1<<PRUSART1);
 }
