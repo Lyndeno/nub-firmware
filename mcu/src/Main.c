5f4a290bd667da6d914163e704bf784b860d58e6
Lyndon Sanche <lsanche@lyndeno.ca>
2021-02-23 14:31:27 -0700

    Test MCU uart output to computer

--- mcu/src/Main.c
+++ mcu/src/Main.c
@@ -2,6 +2,9 @@
 #include "io.h"
 
 #define F_CPU 16000000UL
+#define FOSC 16000000UL
+#define BAUD 9600
+#define UBRR FOSC/16/BAUD-1
 
 #include <util/delay.h>
 #include <stdio.h>
@@ -20,7 +23,20 @@
 #define ADDR_LENGTH 3
 #define MESSAGE_LENGTH 5
 
+void UART_init (unsigned int ubrr) {
+    // Disable power reduction for USARTS
+    //PRR0 |= (1<<PRUSART0)|(1<<PRUSART1);
+
+    // Enable WiFi UART
+    UBRR0H = (unsigned char)(ubrr>>8);
+    UBRR0L = (unsigned char)(ubrr);
+    UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+    // default settings are 1 start bit, 8 data bits, no parity, 1 stop bit
+}
+
 int main (void) {
+    unsigned char data[] = "Hello from ATmega328PB!, this is a test of UART output.\r\n";
+    UART_init(UBRR);
     DDR(PORT_STATUS_LED) |= (1<<PIN_STATUS_LED);
     DDR(PORT_TEST_LED) |= (1<<PIN_TEST_LED);
     while(1) {
@@ -29,20 +45,23 @@ int main (void) {
         _delay_ms(1000);
         PORT(PORT_STATUS_LED) &= ~(1<<PIN_STATUS_LED);
         PORT(PORT_TEST_LED) |= (1<<PIN_TEST_LED);
+        int i = 0;
+        while(data[i] != 0) {
+            while(!( UCSR0A & (1<<UDRE0)));
+            UDR0 = data[i];
+            i++;
+        }
         _delay_ms(1000);
     }
 }
-/*
 
+/*
 void gpio_init (void) {
     
-}
+}*/
 
-void UART_init (void) {
-    // Disable power reduction for USARTS
-    PRR0 |= (1<<PRUSART0)|(1<<PRUSART1);
-}
 
+/*
 // Receive message from WiFi device
 // Call appropriate send function when message destination is determined
 void wifi_receive (void) {
