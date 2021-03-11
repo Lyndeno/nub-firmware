adbfca2c0ce1cc7fd4b6bb76550c3e050431bf51
Lyndon Sanche <lsanche@lyndeno.ca>
2021-03-10 20:12:41 -0700

    Define tranceiver interrupt vector

--- mcu/src/comms.h
+++ mcu/src/comms.h
@@ -12,10 +12,10 @@
 #define RXC_WIFI RXC0
 #define UDR_WIFI UDR0
 #define UDRE_WIFI UDRE0
-#define RXEN_WIFI RXEN0 // Receive enable
-#define TXEN_WIFI TXEN0 // Transmit enable
-#define RXCIE_WIFI RXCIE0 // Receive interrupt enable
-#define WIFI_RX_vect USART0_RX_vect
+#define RXEN_WIFI RXEN0 // WiFi receive enable
+#define TXEN_WIFI TXEN0 // WiFi transmit enable
+#define RXCIE_WIFI RXCIE0 // WiFI receive interrupt enable
+#define WIFI_RX_vect USART0_RX_vect // WiFi receive interrupt vector
 
 // Transceiver
 #define UCSRA_TRANS UCSR1A
@@ -25,9 +25,10 @@
 #define RXC_TRANS RXC1
 #define UDR_TRANS UDR1
 #define UDRE_TRANS UDRE1
-#define RXEN_TRANS RXEN1 // Receive enable
-#define TXEN_TRANS TXEN1 // Transmit enable
-#define RXCIE_TRANS RXCIE1 // Receive interrupt enable
+#define RXEN_TRANS RXEN1 // Transceiver receive enable
+#define TXEN_TRANS TXEN1 // Transceiver transmit enable
+#define RXCIE_TRANS RXCIE1 // Transceiver receive interrupt enable
+#define TRANS_RX_vect USART1_RX_vect // Transceiver receive interrupt vector
 
 // Initialize UART
 void UART_init(unsigned int);
