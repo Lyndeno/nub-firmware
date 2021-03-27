5e996980d8c31cf4817b4457e7a31fb7362b6c25
Lyndon Sanche <lsanche@lyndeno.ca>
2021-03-27 16:47:08 -0600

    Add ip address to frame data

--- wifi/main/routing.h
+++ wifi/main/routing.h
@@ -1,5 +1,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
+#include "lwip/inet.h"
+
 
 extern QueueHandle_t q_uart_rx_bytes; // bytes received from UART
 extern QueueHandle_t q_uart_tx_bytes; // bytes to send through UART
@@ -10,6 +12,7 @@ extern QueueHandle_t q_wifi_tx_frames; // frame of bytes to send through WiFI
 typedef struct message_frame {
     uint8_t *data;
     size_t len;
+    in_addr_t ip_addr;
 } message_frame;
 
 void tx_byte(uint8_t);
