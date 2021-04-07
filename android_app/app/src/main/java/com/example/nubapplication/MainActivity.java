98960641429e5999d98dd75cdd640dcd70078cae
Lyndon Sanche <lsanche@lyndeno.ca>
2021-04-07 16:16:04 -0600

    Move app to main branch

---
+++ android_app/app/src/main/java/com/example/nubapplication/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.nubapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiEnterpriseConfig;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button connect_button;
+
+    Boolean turn;
+    static WifiManager wifiManager;
+    Context context;
+    WifiConfiguration conf;
+    public static String networkSSID = "esp_abc";
+    public static String networkPass = "1234";
+    byte[] buf = new byte[1024];
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        connect_button = (Button) findViewById(R.id.connect_button);
+        connect_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openTextingActivity();
+            }
+        });
+
+        turn= true;
+        context = this;
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+    }
+
+
+
+    public void openTextingActivity() {
+        android.content.Intent intent = new Intent(this, TextingActivity.class);
+        startActivity(intent);
+    }
+
+
+    public void wifi_connect (View v) {
+
+        wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+
+        if (turn) {
+
+            turnOnOffWifi(context, turn);
+            turn = false;
+            Toast.makeText(getApplicationContext(), "turning on...", Toast.LENGTH_SHORT).show();
+
+            conf = new WifiConfiguration();
+            conf.SSID = "\"" + networkSSID + "\"";
+            conf.preSharedKey = "\"" + networkPass + "\"";
+            conf.status = WifiConfiguration.Status.ENABLED;
+            conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+            conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+            conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+            conf.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+            conf.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+            conf.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
+            int netid = wifiManager.addNetwork(conf);
+            wifiManager.disconnect();
+            wifiManager.enableNetwork(netid, true);
+            wifiManager.reconnect();
+
+        }
+        else {
+            turnOnOffWifi(context,turn);
+            turn = true;
+            Toast.makeText(getApplicationContext(), "turning off...", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public static void turnOnOffWifi(Context context, boolean isTurnToOn) {
+        wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        wifiManager.setWifiEnabled(isTurnToOn);
+    }
+
+}
---
+++ android_app/app/src/main/java/com/example/nubapplication/TextingActivity.java
@@ -0,0 +1,102 @@
+package com.example.nubapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class TextingActivity extends AppCompatActivity {
+
+    TextView textResponse;
+    EditText Message;
+    Button sendbutton;
+    String IpAddress = "192.168.4.1";//Ip address of esp
+    int Port = 3333;//Wifi Server Number to be filled out;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_texting);
+
+        sendbutton = (Button) findViewById(R.id.sendMessage);
+        textResponse = (TextView) findViewById(R.id.Response);
+        Message = (EditText) findViewById(R.id.message);
+
+        sendbutton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                MyClientTask myClientTask = new MyClientTask(Message.getText().toString());
+                myClientTask.execute();
+            }
+
+        });
+
+    }
+    @SuppressLint("StaticFieldLeak")
+    public class MyClientTask extends AsyncTask<Void,Void,Void> {
+        String Response = "";
+        String msgToServer;
+
+        MyClientTask(String msgTo) {
+            msgToServer = msgTo;
+        }
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            Socket socket = null;
+            DataOutputStream dataOutputStream = null;
+            DataInputStream dataInputStream = null;
+
+            try {
+                socket = new Socket(IpAddress, Port);
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataInputStream = new DataInputStream(socket.getInputStream());
+
+                if(!msgToServer.equals(""))
+                    dataOutputStream.writeUTF(msgToServer + "$");
+
+                Response = dataInputStream.readUTF();
+
+            }
+            catch (IOException e) { }
+            finally {
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    }
+                    catch (IOException e) {}
+                }
+                if (dataOutputStream != null) {
+                    try {
+                        dataOutputStream.close();
+                    }
+                    catch (IOException e) {}
+                }
+                if (dataInputStream != null) {
+                    try {
+                        dataInputStream.close();
+                    }
+                    catch (IOException e) {}
+                }
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result) {
+            textResponse.setText(Response);
+            super.onPostExecute(result);
+        }
+    }
+}
