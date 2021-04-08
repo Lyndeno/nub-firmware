1615eeb9480cd8fdd65ab09d6973865182fa0cf4
Lyndon Sanche <lsanche@lyndeno.ca>
2021-04-07 19:37:12 -0600

    Define more UART vars

--- mcu/src/comms.h
+++ mcu/src/comms.h
@@ -10,27 +10,34 @@
 // Wifi
 #define UCSRA_WIFI UCSR0A
 #define UCSRB_WIFI UCSR0B
+#define UCSRC_WIFI UCSR0C
 #define UBRRH_WIFI UBRR0H
 #define UBRRL_WIFI UBRR0L
 #define RXC_WIFI RXC0
 #define UDR_WIFI UDR0
 #define UDRE_WIFI UDRE0
+#define U2X_WIFI U2X0
 #define RXEN_WIFI RXEN0 // WiFi receive enable
 #define TXEN_WIFI TXEN0 // WiFi transmit enable
 #define RXCIE_WIFI RXCIE0 // WiFI receive interrupt enable
+#define USBS_WIFI USBS0
+#define UCSZ0_WIFI UCSZ00
 #define WIFI_RX_vect USART0_RX_vect // WiFi receive interrupt vector
-
 // Transceiver
 #define UCSRA_TRANS UCSR1A
 #define UCSRB_TRANS UCSR1B
+#define UCSRC_TRANS UCSR1C
 #define UBRRH_TRANS UBRR1H
 #define UBRRL_TRANS UBRR1L
 #define RXC_TRANS RXC1
 #define UDR_TRANS UDR1
 #define UDRE_TRANS UDRE1
+#define U2X_TRANS U2X1
 #define RXEN_TRANS RXEN1 // Transceiver receive enable
 #define TXEN_TRANS TXEN1 // Transceiver transmit enable
 #define RXCIE_TRANS RXCIE1 // Transceiver receive interrupt enable
+#define USBS_TRANS USBS1
+#define UCSZ0_TRANS UCSZ10
 #define TRANS_RX_vect USART1_RX_vect // Transceiver receive interrupt vector
 
 // Initialize UART
