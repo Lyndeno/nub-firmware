2a664608811d03c24eb699f9918230f015806572
Lyndon Sanche <lsanche@lyndeno.ca>
2021-04-07 19:23:37 -0600

    Add kyle main stuff

--- mcu/src/Main.c
+++ mcu/src/Main.c
@@ -7,6 +7,51 @@
 #include "buffer.h"
 #include "routing.h"
 
+// Kyle Stuff
+
+#define RSTHUM		PORTB6
+#define MODEIND		PORTB7
+
+#define CRESP		PORTD2
+#define PB			PORTD3
+#define CMD			PORTD5
+#define SLEEPMODE	PORTD7
+
+#define LED			PORTE1
+
+extern unreadBytes;
+extern rx0Buffer;
+extern rx0WritePos;
+#define RX_BUFFER_SIZE 256
+#define TX_BUFFER_SIZE 256
+
+#define MaxNetworkSize 5
+#define MAX_NUB_CON 3
+#define MAX_PHONE_CON 3
+
+uint8_t connectedDevices[] = {0x0A, 0x00, 0x12, 0xEF};
+uint8_t *myDSN;
+
+// The specific device connections, 35 Bytes
+struct localConnections{
+	uint8_t deviceDSN[4];
+	uint8_t numOfNubCon;
+	uint8_t numOfPhoneCon;				// How many devices are connected to this one
+	uint8_t nubConnections[MAX_NUB_CON][4];	// The DSN addresses of the connected devices 
+	uint8_t phoneConnections[MAX_PHONE_CON][6];	// The mac addresses of connected phones
+	};
+
+
+// Overall network structure, 175 Bytes
+struct networkStructure{	
+	uint8_t numOfDevices;
+	
+	struct localConnections device[5];		// Max network size of 5 connections 
+	
+};
+
+// End of Kyle stuff
+
 
 // TODO: Match UART to physical implementation, UART0 and UART1
 
@@ -29,4 +74,33 @@ int main (void) {
         read_frame(&buff_wifi_rx);
         
     }
+
+    /*DDRE |= (1 << DDE1);
+	PORTE |= (1 << LED);
+	USART_setup(MYUBRR);
+	sei();		// Enabling interrupts 
+	
+	
+	_delay_ms(100);
+	myDSN = setupTransceiver();
+	struct networkStructure *networkptr, network;
+	networkptr = &network;							// Pointer to network, to be able to transmit through UART
+	
+	
+	
+    while (1) 
+		
+    {	
+		//TXWrite(var,6,0);
+		//TXWrite(networkptr,sizeof(network),0);
+		broadcastCon(0x01,myDSN,networkptr,sizeof(network));
+		//broadcastCon(0x9,myDSN,networkptr);
+		//sendMessageSimple(myDSN,connectedDevices,message,5);
+		_delay_ms(5000);
+		
+    }*/
+}
+
+void syncNetwork(){
+	
 }
