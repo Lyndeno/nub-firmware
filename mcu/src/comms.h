6b6520acc9d487e39b0ceedc3eedb341f53ac9b3
Lyndon Sanche <lsanche@lyndeno.ca>
2021-04-13 00:48:21 -0600

    Swap UART ports

--- mcu/src/comms.h
+++ mcu/src/comms.h
@@ -8,30 +8,30 @@
 
 // define USART registers
 // Wifi
-#define UCSRA_WIFI UCSR0A
-#define UCSRB_WIFI UCSR0B
-#define UBRRH_WIFI UBRR0H
-#define UBRRL_WIFI UBRR0L
-#define RXC_WIFI RXC0
-#define UDR_WIFI UDR0
-#define UDRE_WIFI UDRE0
-#define RXEN_WIFI RXEN0 // WiFi receive enable
-#define TXEN_WIFI TXEN0 // WiFi transmit enable
-#define RXCIE_WIFI RXCIE0 // WiFI receive interrupt enable
-#define WIFI_RX_vect USART0_RX_vect // WiFi receive interrupt vector
+#define UCSRA_WIFI UCSR1A
+#define UCSRB_WIFI UCSR1B
+#define UBRRH_WIFI UBRR1H
+#define UBRRL_WIFI UBRR1L
+#define RXC_WIFI RXC1
+#define UDR_WIFI UDR1
+#define UDRE_WIFI UDRE1
+#define RXEN_WIFI RXEN1 // WiFi receive enable
+#define TXEN_WIFI TXEN1 // WiFi transmit enable
+#define RXCIE_WIFI RXCIE1 // WiFI receive interrupt enable
+#define WIFI_RX_vect USART1_RX_vect // WiFi receive interrupt vector
 
 // Transceiver
-#define UCSRA_TRANS UCSR1A
-#define UCSRB_TRANS UCSR1B
-#define UBRRH_TRANS UBRR1H
-#define UBRRL_TRANS UBRR1L
-#define RXC_TRANS RXC1
-#define UDR_TRANS UDR1
-#define UDRE_TRANS UDRE1
-#define RXEN_TRANS RXEN1 // Transceiver receive enable
-#define TXEN_TRANS TXEN1 // Transceiver transmit enable
-#define RXCIE_TRANS RXCIE1 // Transceiver receive interrupt enable
-#define TRANS_RX_vect USART1_RX_vect // Transceiver receive interrupt vector
+#define UCSRA_TRANS UCSR0A
+#define UCSRB_TRANS UCSR0B
+#define UBRRH_TRANS UBRR0H
+#define UBRRL_TRANS UBRR0L
+#define RXC_TRANS RXC0
+#define UDR_TRANS UDR0
+#define UDRE_TRANS UDRE0
+#define RXEN_TRANS RXEN0 // Transceiver receive enable
+#define TXEN_TRANS TXEN0 // Transceiver transmit enable
+#define RXCIE_TRANS RXCIE0 // Transceiver receive interrupt enable
+#define TRANS_RX_vect USART0_RX_vect // Transceiver receive interrupt vector
 
 // Initialize UART
 
